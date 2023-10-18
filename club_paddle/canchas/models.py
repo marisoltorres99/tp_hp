907c41fd72095a744b4778cfe86367a4b798ea44
marisol <marisooltorrees@gmail.com>
2023-10-18 13:28:48 -0300

    creacion app canchas y modelo

---
+++ club_paddle/canchas/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Cancha(models.Model):
+    cancha_id = models.BigAutoField(primary_key=True)
+    valoracion = models.DecimalField(max_digits=1, decimal_places=1, null=True, blank=True)
+    activo = models.BooleanField(default=True)
+    numero = models.IntegerField()
+
+    class Meta:
+        verbose_name="cancha"
+        verbose_name_plural="canchas"
+        db_table="canchas"
+

b45c3ce02a4360e5e4766e6b3df8db40f43e7a24
marisol <marisooltorrees@gmail.com>
2023-11-30 15:53:24 -0300

    corrijo errores de migracion, agrego el cambio de estado de la cancha, comienzo la opcion de editar cancha

--- club_paddle/canchas/urls.py
+++ club_paddle/canchas/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.abm_canchas, name="Canchas"),
-    path('nueva/', views.nueva_cancha, name="Nueva"),
+    path("", views.abm_canchas, name="Canchas"),
+    path("nueva/", views.nueva_cancha, name="Nueva"),
+    path("editar/<int:cancha_id>/", views.editar_cancha, name="EditarCancha"),
 ]
--- club_paddle/canchas/views.py
+++ club_paddle/canchas/views.py
@@ -1,13 +1,26 @@
 from django.contrib import messages
 from django.shortcuts import HttpResponseRedirect, render
+from django.urls import reverse
 
 from canchas.forms import FormNuevaCancha
 from canchas.models import Cancha, CanchaPrecios, HorariosCancha
 
 
 def abm_canchas(request):
-    canchas = Cancha.objects.all()
-    return render(request, "canchas/abm_canchas.html", {"canchas": canchas})
+    if request.method == "GET":
+        canchas = Cancha.objects.all()
+        context = {"canchas": canchas}
+        return render(request, "canchas/abm_canchas.html", context)
+    else:
+        if "desactivar" in request.POST:
+            cancha_id = request.POST.get("desactivar")
+            cancha_qs = Cancha.objects.filter(cancha_id=cancha_id)
+            cancha_qs.update(activo=False)
+        else:
+            cancha_id = request.POST.get("activar")
+            cancha_qs = Cancha.objects.filter(cancha_id=cancha_id)
+            cancha_qs.update(activo=True)
+        return HttpResponseRedirect(reverse("Canchas"))
 
 
 def nueva_cancha(request):
@@ -52,9 +65,44 @@ def nueva_cancha(request):
         mi_formulario = FormNuevaCancha()
     return render(
         request,
-        "canchas/nueva_cancha.html",
+        "canchas/form_cancha.html",
         {
             "form": mi_formulario,
             "dias": dias,
+            "boton_submit": "Cargar",
+            "abm": "Nueva Cancha",
         },
     )
+
+
+def editar_cancha(request, **kwargs):
+    if request.method == "GET":
+        cancha = Cancha.objects.get(cancha_id=kwargs["cancha_id"])
+        datos_iniciales = {
+            "numero": cancha.numero,
+            "precio": cancha.obtener_precio_actual,
+        }
+        mi_formulario = FormNuevaCancha(initial=datos_iniciales)
+        context = {
+            "form": mi_formulario,
+            "boton_submit": "Modificar",
+            "abm": "Editar Cancha",
+        }
+        return render(request, "canchas/form_cancha.html", context)
+    else:
+        mi_formulario = FormNuevaCancha(request.POST)
+        if mi_formulario.is_valid():
+            numero = mi_formulario.cleaned_data["numero"]
+            precio = mi_formulario.cleaned_data["precio"]
+
+            cancha_id = kwargs["cancha_id"]
+            cancha_qs = Cancha.objects.filter(cancha_id=cancha_id)
+            cancha_qs.update(numero=numero)
+
+            cancha = Cancha.objects.get(cancha_id=kwargs["cancha_id"])
+            nuevo_precio = CanchaPrecios(cancha=cancha, precio=precio)
+            nuevo_precio.save()
+
+            messages.success(request, "¡Cancha modificada con éxito!")
+        url_destino = reverse("EditarCancha", kwargs={"cancha_id": cancha_id})
+        return HttpResponseRedirect(url_destino)

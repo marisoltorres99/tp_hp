8a3ed3125b0433d1ec2adbf48677a4451ee5f967
marisol <marisooltorrees@gmail.com>
2024-01-11 11:43:55 -0300

    completo el editar cancha

--- club_paddle/canchas/views.py
+++ club_paddle/canchas/views.py
@@ -10,10 +10,12 @@ from canchas.models import Cancha, CanchaPrecios, HorariosCancha
 
 def abm_canchas(request):
     if request.method == "GET":
+        # busco canchas existentes para mostrar la tabla
         canchas = Cancha.objects.all()
         context = {"canchas": canchas}
         return render(request, "canchas/abm_canchas.html", context)
     else:
+        # cambio de estado de cancha
         if "desactivar" in request.POST:
             cancha_id = request.POST.get("desactivar")
             cancha_qs = Cancha.objects.filter(cancha_id=cancha_id)
@@ -44,21 +46,25 @@ def nueva_cancha(request):
         return render(request, "canchas/nueva_cancha.html", context)
 
     else:
+        # recupero datos del form
         mi_formulario = FormNuevaCancha(request.POST)
         if mi_formulario.is_valid():
             numero = mi_formulario.cleaned_data["numero"]
             precio = mi_formulario.cleaned_data["precio"]
+            # creo nueva cancha y nuevo precio para la cancha
             cancha = Cancha(numero=numero)
             cancha.save()
             cancha_precio = CanchaPrecios(cancha=cancha, precio=precio)
             cancha_precio.save()
 
+            # recupero datos del form y elimino lo que no sea parte de los horarios
             datos_formulario = request.POST.dict()
 
             for key in ["csrfmiddlewaretoken", "numero", "precio"]:
                 if key in datos_formulario:
                     del datos_formulario[key]
 
+            # cargo los horarios para la nueva cancha
             for dia, valor in datos_formulario.items():
                 if valor == "on":
                     cancha_horario = HorariosCancha(cancha=cancha, dia=dia)
@@ -83,6 +89,7 @@ def editar_cancha(request, **kwargs):
     dias["Domingo"] = {"obj": None, "hora": "horaDomingo"}
 
     if request.method == "GET":
+        # busco datos existentes de la cancha para mostrarlos
         cancha = Cancha.objects.get(cancha_id=kwargs["cancha_id"])
 
         horarios_qs = HorariosCancha.objects.filter(cancha_id=kwargs["cancha_id"])
@@ -100,11 +107,13 @@ def editar_cancha(request, **kwargs):
         }
         return render(request, "canchas/editar_cancha.html", context)
     else:
+        # obtengo datos del form
         mi_formulario = FormNuevaCancha(request.POST)
         if mi_formulario.is_valid():
             numero = mi_formulario.cleaned_data["numero"]
             precio = mi_formulario.cleaned_data["precio"]
 
+            # actualizo numero y precio
             cancha_id = kwargs["cancha_id"]
             cancha_qs = Cancha.objects.filter(cancha_id=cancha_id)
             cancha_qs.update(numero=numero)
@@ -115,24 +124,31 @@ def editar_cancha(request, **kwargs):
                 nuevo_precio = CanchaPrecios(cancha=cancha, precio=precio)
                 nuevo_precio.save()
 
+            # obtengo datos del form para actualizar los horarios
             datos_formulario = request.POST.dict()
 
-            """for key in ["csrfmiddlewaretoken", "numero", "precio"]:
+            # elimino de los datos del form las claves que no sean parte de los horarios
+            for key in ["csrfmiddlewaretoken", "numero", "precio"]:
                 if key in datos_formulario:
                     del datos_formulario[key]
 
+            print(datos_formulario)
+
+            # busco horarios existentes
+            horarios_qs = HorariosCancha.objects.filter(cancha_id=kwargs["cancha_id"])
+
+            # borro horarios existentes
+            horarios_qs.delete()
+
+            # cargo los nuevos horarios
             for dia, valor in datos_formulario.items():
                 if valor == "on":
-                    horarios_cancha_qs = HorariosCancha.objects.filter(
-                        cancha_id=cancha_id
-                    )
+                    cancha_horario = HorariosCancha(cancha=cancha, dia=dia)
                     desde_key = f"hora{dia}_desde"
                     hasta_key = f"hora{dia}_hasta"
-                    horarios_cancha_qs.update(
-                        dia=dia,
-                        hora_desde=datos_formulario.get(desde_key),
-                        hora_hasta=datos_formulario.get(hasta_key),
-                    )"""
+                    cancha_horario.hora_desde = datos_formulario.get(desde_key)
+                    cancha_horario.hora_hasta = datos_formulario.get(hasta_key)
+                    cancha_horario.save()
 
             messages.success(request, "¡Cancha modificada con éxito!")
         url_destino = reverse("EditarCancha", kwargs={"cancha_id": cancha_id})

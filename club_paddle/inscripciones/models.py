01491b310786e1224508b089d0027b86af80a491
marisol <marisooltorrees@gmail.com>
2024-01-17 10:53:16 -0300

    creo modelo inscripcion

--- club_paddle/inscripciones/models.py
+++ club_paddle/inscripciones/models.py
@@ -1,3 +1,26 @@
+from clases.models import Clase
 from django.db import models
+from usuarios.models import Cliente
 
-# Create your models here.
+
+class Inscripcion(models.Model):
+    inscripcion_id = models.BigAutoField(primary_key=True)
+    fecha_hora_inscripcion = models.DateTimeField(null=False, blank=False)
+    cliente = models.ForeignKey(
+        Cliente,
+        on_delete=models.RESTRICT,
+        related_name="inscripciones",
+    )
+    clase = models.ForeignKey(
+        Clase,
+        on_delete=models.RESTRICT,
+        related_name="inscripciones",
+    )
+
+    class Meta:
+        verbose_name = "inscripcion"
+        verbose_name_plural = "inscripciones"
+        db_table = "inscripciones"
+
+    def __str__(self):
+        return self.inscripcion_id.__str__()

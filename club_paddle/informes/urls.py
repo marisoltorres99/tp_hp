3e42140a2cfaacbb8acb86b782124e48ff439eaf
marisol <marisooltorrees@gmail.com>
2024-01-24 21:13:14 -0300

    creo urls y vistas para los 3 tipos de informes

--- club_paddle/informes/urls.py
+++ club_paddle/informes/urls.py
@@ -3,6 +3,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("elegir/", views.elegir_informe, name="elegir_informe"),
-    path("elegir_fecha/", views.elegir_fecha, name="elegir_fecha"),
+    path("elegir_informe/", views.elegir_informe, name="elegir_informe"),
+    path(
+        "elegir_fecha_canchas_reservadas/",
+        views.elegir_fecha_canchas_reservadas,
+        name="elegir_fecha_canchas_reservadas",
+    ),
+    path(
+        "elegir_fecha_reservas_canceladas/",
+        views.elegir_fecha_reservas_canceladas,
+        name="elegir_fecha_reservas_canceladas",
+    ),
+    path(
+        "elegir_fecha_clases_solicitadas/",
+        views.elegir_fecha_clases_solicitadas,
+        name="elegir_fecha_clases_solicitadas",
+    ),
 ]
--- club_paddle/informes/views.py
+++ club_paddle/informes/views.py
@@ -6,6 +6,16 @@ def elegir_informe(request):
         return render(request, "informes/elegir_informe.html")
 
 
-def elegir_fecha(request):
+def elegir_fecha_canchas_reservadas(request):
+    if request.method == "GET":
+        return render(request, "informes/elegir_fecha.html")
+
+
+def elegir_fecha_reservas_canceladas(request):
+    if request.method == "GET":
+        return render(request, "informes/elegir_fecha.html")
+
+
+def elegir_fecha_clases_solicitadas(request):
     if request.method == "GET":
         return render(request, "informes/elegir_fecha.html")

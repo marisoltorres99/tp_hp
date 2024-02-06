d7383270952e8988e061f3150669db1e1cbac1e5
marisol <marisooltorrees@gmail.com>
2024-02-06 19:37:46 -0300

    contemplo el ingreso de una valoracion no numerica y correcion al mostrar mensaje de error

--- club_paddle/valoraciones/views.py
+++ club_paddle/valoraciones/views.py
@@ -9,28 +9,36 @@ from valoraciones.models import Valoracion
 
 
 def nueva_valoracion(request, cancha_id):
+    cancha = Cancha.objects.get(cancha_id=cancha_id)
+    valoracion_cliente = Valoracion.objects.filter(
+        cliente_id=request.user.id, cancha=cancha
+    ).first()
+    if valoracion_cliente:
+        mensaje = f"Ya realizo una valoracion para esta cancha. Valoracion actual: {valoracion_cliente.valoracion}. Reemplazar Valoracion:"
+    else:
+        mensaje = "¡Danos tu opinión! (1 a 5)"
+
     if request.method == "GET":
-        cancha = Cancha.objects.get(cancha_id=cancha_id)
-        valoracion_cliente = Valoracion.objects.filter(
-            cliente_id=request.user.id, cancha=cancha
-        ).first()
-        if valoracion_cliente is not None:
-            mensaje = f"Ya realizo una valoracion para esta cancha. Valoracion actual: {valoracion_cliente.valoracion}. Reemplazar Valoracion:"
-        else:
-            mensaje = "¡Danos tu opinión! (1 a 5)"
         return render(
             request, "valoraciones/valorar.html", {"cancha": cancha, "mensaje": mensaje}
         )
     else:
         valoracion_str = request.POST.get("valoracion")
-        valoracion_int = int(valoracion_str)
-        cancha = Cancha.objects.get(cancha_id=cancha_id)
+        try:
+            valoracion_int = int(valoracion_str)
+        except ValueError:
+            messages.error(request, "Ingrese una valoracion correcta")
+            return render(
+                request,
+                "valoraciones/valorar.html",
+                {"cancha": cancha, "mensaje": mensaje},
+            )
         if valoracion_int >= 1 and valoracion_int <= 5:
             cliente = Cliente.objects.get(user_id=request.user.id)
             valoracion_cliente = Valoracion.objects.filter(
                 cliente_id=request.user.id, cancha=cancha
             ).first()
-            if valoracion_cliente is None:
+            if not valoracion_cliente:
                 nueva_val = Valoracion(
                     valoracion=valoracion_int,
                     cliente=cliente,
@@ -47,5 +55,9 @@ def nueva_valoracion(request, cancha_id):
 
             return HttpResponseRedirect(reverse("mis_reservas"))
         else:
-            messages.success(request, "Ingrese una valoracion correcta")
-            return render(request, "valoraciones/valorar.html", {"cancha": cancha})
+            messages.error(request, "Ingrese una valoracion correcta")
+            return render(
+                request,
+                "valoraciones/valorar.html",
+                {"cancha": cancha, "mensaje": mensaje},
+            )

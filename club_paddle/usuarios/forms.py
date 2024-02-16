a871321e378d7940a9c18d04f7cdff9beefb7a81
marisol <marisooltorrees@gmail.com>
2024-02-16 11:27:22 -0300

    mejoro interfaz de cambio de contraseña

--- club_paddle/usuarios/forms.py
+++ club_paddle/usuarios/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import (
     AuthenticationForm,
+    PasswordChangeForm,
     UserChangeForm,
     UserCreationForm,
     UsernameField,
@@ -153,3 +154,11 @@ class FormModificarCliente(UserChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields.pop("password", None)
+
+
+class FormCambiarPassword(PasswordChangeForm):
+    def __init__(self, user, *args, **kwargs):
+        super(FormCambiarPassword, self).__init__(user, *args, **kwargs)
+        self.fields["old_password"].widget.attrs.update({"class": "form-control"})
+        self.fields["new_password1"].widget.attrs.update({"class": "form-control"})
+        self.fields["new_password2"].widget.attrs.update({"class": "form-control"})
